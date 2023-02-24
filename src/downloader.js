86b127dec276cd5978122d6a5c06b52e8322bb1d
Sijawusz Pur Rahnama <sija@sija.pl>
2023-02-24 23:04:04 +0100

    Refactor error handling

--- src/downloader.js
+++ src/downloader.js
@@ -32,107 +32,107 @@ export class Downloader {
   }
 
   async run(targetURL) {
-    // Chrome / Chromium cuts off lines in half when margins are applied, see
-    // https://github.com/puppeteer/puppeteer/issues/8734#issuecomment-1234332415
-    this.browser = await puppeteer.launch({
-      product: 'firefox'
-    })
-    const page = await this.browser.newPage()
-
-    this.#logger.info('Visiting "%s"', chalk.green(targetURL))
-
     try {
+      // Chrome / Chromium cuts off lines in half when margins are applied, see
+      // https://github.com/puppeteer/puppeteer/issues/8734#issuecomment-1234332415
+      this.browser = await puppeteer.launch({
+        product: 'firefox'
+      })
+      const page = await this.browser.newPage()
+
+      this.#logger.info('Visiting "%s"', chalk.green(targetURL))
+
       const response = await page.goto(targetURL, {
         waitUntil: 'load',
         timeout: this.settings.timeout,
       })
+
       if (!response.ok()) {
-        this.#logger.fatal('Received error response: "%s"', chalk.red(response.statusText()))
-        throw new Error(`Received error response: "${response.statusText()}"`)
-      }
-    } catch (e) {
-      this.#logger.fatal('Failed: %s', chalk.red(e))
-      throw e
-    }
-
-    await page.evaluate(() => {
-      // Expand all TOC menu items, so we have all of the links present in DOM
-      const elements = document
-        .querySelectorAll('a[data-rnwrdesktop-fnigne="true"] > div[tabindex="0"]')
-
-      for (let element of elements) {
-        element.click()
-      }
-    })
-
-    const content = await page.content()
-    const links = this.#collectLinks(content)
-
-    await page.close()
-
-    this.#logger.info('Links collected: %O', links)
-
-    for (let href of links) {
-      let slug = this.#href2slug(href)
-
-      switch (slug) {
-        case '':
-          this.#logger.warn('Empty slug, ignoring "%s"', chalk.green(href))
-          continue
-        case '/':
-          slug = 'index'
-          break
+        throw new Error(`${response.statusText()} (${response.status()})`)
       }
 
-      const outPath = path.join(this.settings.outDir, `${slug}.pdf`)
-      const outDir = path.dirname(outPath)
+      await page.evaluate(() => {
+        // Expand all TOC menu items, so we have all of the links present in DOM
+        const elements = document
+          .querySelectorAll('a[data-rnwrdesktop-fnigne="true"] > div[tabindex="0"]')
 
-      // mkdir -p <path>
-      await fs.mkdir(outDir, {
-        recursive: true
+        for (let element of elements) {
+          element.click()
+        }
       })
 
-      const url = new URL(href, targetURL)
+      const content = await page.content()
+      const links = this.#collectLinks(content)
 
-      try {
-        await this.#downloadPage(url, outPath, async page => {
-          await page.evaluate(() => {
-            // Expand all expandable sections
-            const sectionsToExpand = document
-              .querySelectorAll('div[aria-controls^="expandable-body-"]')
+      await page.close()
 
-            for (let section of sectionsToExpand) {
-              section.click()
-            }
+      this.#logger.info('Links collected: %O', links)
 
-            // Remove redundant/interactive elements
-            const itemSelectorsToRemove = [
-              'header + div[data-rnwrdesktop-hidden="true"]',
-              'div[aria-label="Search…"]',
-              'div[aria-label="Page actions"]',
-            ]
-            const itemsToRemove = document
-              .querySelectorAll(itemSelectorsToRemove.join(', '))
+      for (let href of links) {
+        let slug = this.#href2slug(href)
 
-            for (let item of itemsToRemove) {
-              item.remove()
-            }
+        switch (slug) {
+          case '':
+            this.#logger.warn('Empty slug, ignoring "%s"', chalk.green(href))
+            continue
+          case '/':
+            slug = 'index'
+            break
+        }
 
-            // Turn relative timestamps into absolute ones
-            const lastModifiedEl = document
-              .querySelector('div[dir="auto"] > span[aria-label]')
+        const outPath = path.join(this.settings.outDir, `${slug}.pdf`)
+        const outDir = path.dirname(outPath)
 
-            if (lastModifiedEl) {
-              lastModifiedEl.innerHTML = lastModifiedEl.getAttribute('aria-label')
-            }
-          })
+        // mkdir -p <path>
+        await fs.mkdir(outDir, {
+          recursive: true
         })
-      } catch (e) {
-        this.#logger.error('Downloading "%s" failed: %s', chalk.green(url), chalk.red(e))
-      }
-    }
 
-    await this.browser.close()
+        const url = new URL(href, targetURL)
+
+        try {
+          await this.#downloadPage(url, outPath, async page => {
+            await page.evaluate(() => {
+              // Expand all expandable sections
+              const sectionsToExpand = document
+                .querySelectorAll('div[aria-controls^="expandable-body-"]')
+
+              for (let section of sectionsToExpand) {
+                section.click()
+              }
+
+              // Remove redundant/interactive elements
+              const itemSelectorsToRemove = [
+                'header + div[data-rnwrdesktop-hidden="true"]',
+                'div[aria-label="Search…"]',
+                'div[aria-label="Page actions"]',
+              ]
+              const itemsToRemove = document
+                .querySelectorAll(itemSelectorsToRemove.join(', '))
+
+              for (let item of itemsToRemove) {
+                item.remove()
+              }
+
+              // Turn relative timestamps into absolute ones
+              const lastModifiedEl = document
+                .querySelector('div[dir="auto"] > span[aria-label]')
+
+              if (lastModifiedEl) {
+                lastModifiedEl.innerHTML = lastModifiedEl.getAttribute('aria-label')
+              }
+            })
+          })
+        } catch (e) {
+          this.#logger.error('Downloading "%s" failed: %s', chalk.green(url), chalk.red(e))
+        }
+      }
+
+      await this.browser.close()
+    } catch (e) {
+      this.#logger.fatal(e.toString())
+      throw e
+    }
   }
 
   async #downloadPage(url, path, callback = null) {
